191232acaf2f86d6eb5be65b9c551c511e32feb2
Aliossandro <ap1a14@soton.ac.uk>
2017-10-03 17:31:51 +0100

    first commit

---
+++ fileWriter.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import datetime
+
+
+class OntologyFile:
+
+    def __init__(self, outputFile):
+
+        self.output = outputFile
+        # self.dataFilter = dataFilter
+        # self.propertyTypes = {}
+        # self.propertyDeclarationQueue = []
+        # self.filterName = self.dataFilter.getHashCode()
+        # # Keep some statistics (inserted at end of file):
+        # self.entityCount = 0
+        # self.propertyCount = 0  # number of OWL property declarations, not of Wikidata properties
+        # self.propertyLookupCount = 0  # number of additional online lookups
+        # self.statStatementCount = 0
+        # self.statReferenceCount = 0
+        # self.statStmtPropertyCounts = {}
+        # self.statStmtTypeCounts = {}
+        # self.statQualiPropertyCounts = {}
+        # self.statQualiTypeCounts = {}
+        # self.statRefPropertyCounts = {}
+        # self.statRefTypeCounts = {}
+        # self.statTripleCount = 0
+
+        # Make header:
+        self.output.write('### Wikidata OWL/RDF Ontology\n')
+        # self.output.write('# Filter settings (' + self.filterName + ')\n')
+        # for infostr in self.dataFilter.getFilterSettingsInfo():
+        #     self.output.write('# - ' + infostr + '\n')
+
+        self.output.write('# Generated on ' + str(datetime.datetime.now()) + '\n###\n\n')
+
+        self.output.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        self.output.write('<rdf:RDF\n')
+        self.output.write('xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"\n')
+        self.output.write('xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"\n')
+        self.output.write('xmlns:owl="http://www.w3.org/2002/07/owl#"\n')
+        self.output.write('xmlns:dc="http://purl.org/dc/elements/1.1/"\n')
+        self.output.write('xmlns: dcterms = "http://purl.org/dc/terms/"\n')
+        self.output.write('xmlns:d0="http://www.ontologydesignpatterns.org/ont/d0.owl#"\n')
+        self.output.write('xmlns:wikibase="http://wikiba.se/ontology#"\n')
+        self.output.write('xmlns:wd="<http://www.wikidata.org/entity/">\n')
+        self.output.write('<!-- OWL Header -->\n')
+        self.output.write('<owl:Ontology rdf:about="http://www.wikidata.org">\n')
+        self.output.write('<dc:title>Wikidata ontology</dc:title>\n')
+        self.output.write('<dc:description>OWL ontology extracted from Wikidata dumps</dc:description>\n')
+        self.output.write('</owl:Ontology>\n\n')
+
+    def finalWriter(self, propertyAll):
+    ###WRITE PROPERTY FILE
+        propertyTranslated = '\n'.join(propertyAll)
+        self.output.write(propertyTranslated)
+        self.output.write('</rdf:RDF>\n')
+        self.output.close()
---
+++ owlExtractor.py
@@ -0,0 +1,454 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import ujson
+import re
+import bz2
+import os
+import fileWriter
+
+###classes list generator
+def classesGenerator(**args):
+    classesList = []
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(__location__, 'classes.json'), 'r') as f:
+        resultClasses = ujson.load(f)
+        for binding in resultClasses['results']['bindings']:
+            classUri = binding['cl']['value']
+            if classUri[0:31] == 'http://www.wikidata.org/entity/':
+                classId = classUri[31:]
+                classesList.append(classId)
+
+    return classesList
+
+
+def resourceNamer(resource):
+    resourceUri = "http://www.wikidata.org/entity/" + resource
+
+    return resourceUri
+
+def rangeLine(resource):
+    resourceUri = resourceNamer(resource)
+    rangeOutput = '<rdfs:range rdf:resource="' + resourceUri + '"/>'
+
+    return rangeOutput
+
+def rangeMultiple(resource):
+    resourceUri = resourceNamer(resource)
+    rangeOutput = '<owl:Class rdf:resource="' + resourceUri + '"/>'
+
+    return rangeOutput
+
+def domainLine(resource):
+    resourceUri = resourceNamer(resource)
+    domainOutput = '<rdfs:domain rdf:resource="' + resourceUri + '"/>'
+
+    return domainOutput
+
+def collectionItems(resource):
+    individualUri = resourceNamer(resource)
+    individualOutput = '<owl:Thing rdf:about="' + individualUri + '"/>'
+
+    return individualOutput
+
+def disjointUnionClasses(object):
+    disjointUnionOf = "http://www.wikidata.org/entity/" + object
+    resourceDisjointUnionOf = '<owl:Class rdf:resource="' + disjointUnionOf + '"/>'
+
+    return resourceDisjointUnionOf
+
+
+###property extractor
+class entityExtractor:
+
+    def __init__(self, lineParsed):
+        self.lineParsed = lineParsed
+
+
+
+def propertyExtractor(lineParsed):
+    otherKeys = []
+    constraintKeys = []
+
+    try:
+        resourceName = resourceNamer(lineParsed['id'])
+    except:
+        print(lineParsed)
+
+    if lineParsed['datatype'] == 'wikibase-item':
+        propertyType = "ObjectProperty"
+    else:
+        propertyType = "DatatypeProperty"
+
+    propertyDeclaration = '<owl:' + propertyType + ' rdf:about="' + resourceName + '">'
+    propertyDeclarationClosure = '</owl:' + propertyType + '>'
+    resourceType = '<rdf:type rdf:resource="http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"/>'
+    resourceType2 = '<rdf:type rdf:resource="http://wikiba.se/ontology-beta#Property"/>'
+    try:
+        resourceLabel = '<rdfs:label xml:lang="en">' + lineParsed['labels']['en']['value'] + '</rdfs:label>'
+    except KeyError:
+        resourceLabel = '<rdfs:label xml:lang="en">no English label available</rdfs:label>'
+
+    try:
+        resourceDescription = '<schema:description xml:lang="en">' + lineParsed['descriptions']['en'][
+            'value'] + '</schema:description>'
+    except KeyError:
+        resourceDescription = '<schema:description xml:lang="en">no English description available</schema:description>'
+
+    # analyse claims
+    # print(lineParsed['claims'].keys())
+    for key in lineParsed['claims']:
+        if key == 'P31':
+            resourceInstanceList = []
+            for i in lineParsed['claims']['P31']:
+                instanceOf = i['mainsnak']['datavalue']['value']['id']
+                instanceOf = "http://www.wikidata.org/entity/" + instanceOf
+                resourceInstanceOf = '<rdf:type rdf:resource="' + instanceOf + '"/>'
+                resourceInstanceList.append(resourceInstanceOf)
+
+        elif key == 'P1659':
+            resourceSeeList = []
+            for i in lineParsed['claims']['P1659']:
+                seeAlso = i['mainsnak']['datavalue']['value']['id']
+                seeAlso = "http://www.wikidata.org/entity/" + seeAlso
+                resourceSeeAlso = '<rdfs:seeAlso rdf:resource="' + seeAlso + '"/>'
+                resourceSeeList.append(resourceSeeAlso)
+
+        elif key == 'P1647':
+            resourceSubPropertyList = []
+            for i in lineParsed['claims']['P1647']:
+                subPropertyOf = i['mainsnak']['datavalue']['value']['id']
+                subPropertyOf = "http://www.wikidata.org/entity/" + subPropertyOf
+                resourceSubPropertyOf = '<rdfs:subPropertyOf rdf:resource="' + subPropertyOf + '"/>'
+                resourceSubPropertyList.append(resourceSubPropertyOf)
+
+        elif key == 'P1628':
+            resourceEquivalentList = []
+            for i in lineParsed['claims']['P1628']:
+                try:
+                    equivalentProperty = i['mainsnak']['datavalue']['value']['id']
+                    equivalentProperty = "http://www.wikidata.org/entity/" + equivalentProperty
+                except TypeError:
+                    equivalentProperty = i['mainsnak']['datavalue']['value']
+
+                resourceEquivalentProperty = '<owl:equivalentProperty rdf:resource="' + equivalentProperty + '"/>'
+                resourceEquivalentList.append(resourceEquivalentProperty)
+
+        elif key == 'P1696':
+            resourceInverseList = []
+            for i in lineParsed['claims']['P1696']:
+                try:
+                    inverseProperty = i['mainsnak']['datavalue']['value']['id']
+                    inverseProperty = "http://www.wikidata.org/entity/" + inverseProperty
+                except TypeError:
+                    inverseProperty = i['mainsnak']['datavalue']['value']
+
+                resourceInverseProperty = '<owl:inverseOf rdf:resource="' + inverseProperty + '"/>'
+                resourceInverseList.append(resourceInverseProperty)
+
+        # elif key == 'P3254':
+        #
+        # elif key == 'P1793': ###format as regex
+
+        ###property constraints
+        elif key == 'P2302':
+            constraintList = []
+            for i in lineParsed['claims']['P2302']:
+                # print(i['mainsnak']['datavalue']['value']['id'], resourceName)
+                constraintKeys.append(i['mainsnak']['datavalue']['value']['id'])
+                # Q21510865 'value type', range
+                if i['mainsnak']['datavalue']['value']['id'] == 'Q21510865':
+                    domainList = []
+                    relation = [x['datavalue']['value']['id'] for x in i['qualifiers']['P2309']]
+                    # Q21503252 instance of for constraints
+                    if len(relation) == 1:
+                        relation = relation[0]
+                        if relation == 'Q21503252':
+                            rangeTemp = '<rdfs:range rdf:resource="'
+                        else:
+                            print(relation, 'relation type')
+                            # check if other qualifiers are used; what to do with them?
+                            # break
+                    else:
+                        print(relation, 'relation type')
+                        # check if there are other qualifiers; what to do with them?
+                        # break
+
+                    classRange = [y['datavalue']['value']['id'] for y in i['qualifiers']['P2308']]
+                    # classRange = map(rangeLine, classRange)
+                    # constraintList += list(classRange)
+
+                # Q21503250 'type', domain
+                elif i['mainsnak']['datavalue']['value']['id'] == 'Q21503250':
+                    relation = [x['datavalue']['value']['id'] for x in i['qualifiers']['P2309']]
+                    # Q21503252 instance of for constraints
+                    if len(relation) == 1:
+                        relation = relation[0]
+                        if relation == 'Q21503252':
+                            rangeTemp = '<rdfs:domain rdf:resource="'
+                        else:
+                            print(relation, 'relation type')
+                            # check if other qualifiers are used; what to do with them?
+                            # break
+                    else:
+                        print(relation, 'relation type')
+                        # check if there are other qualifiers; what to do with them?
+                        # break
+
+
+                    if 'P2308' in i['qualifiers'].keys():
+                        try:
+                            classDomain = [y['datavalue']['value']['id'] for y in i['qualifiers']['P2308']]
+                        except:
+                            print(i['qualifiers']['P2308'])
+                    else:
+                        print(i['qualifiers'].keys())
+                    # classDomain = map(domainLine, classDomain)
+                    # constraintList += list(classDomain)
+
+                # Q21510851 "allowed qualifiers" constraint ### what to do with it?
+                # Q21502838 "conflicts-with" constraint ### what to do with it?
+
+                # Q21510859 "one-of" constraint ###owl:oneOf: subject must be class, object must be list
+                elif i['mainsnak']['datavalue']['value']['id'] == 'Q21510859':
+                    classOneOf = []
+                    for y in i['qualifiers']['P2305']:
+                        try:
+                            classOneOf.append(y['datavalue']['value']['id'])
+                            ###it can be 'no value' in snaktype
+                        except:
+                            print(i['qualifiers']['P2305'])
+                    classOneOf = map(collectionItems, classOneOf)
+                    # try:
+                    #     classOneOf = [y['datavalue']['value']['id'] for y in i['qualifiers']['P2305']]
+                    #     classOneOf = map(collectionItems, classOneOf)
+                    # except:
+                    #     print(i['qualifiers']['P2305'])
+
+
+                   # Q25796498 "contemporary constraint: subject and object must exist at the same point in time; How do we specify that?
+
+            if 'classRange' in locals():
+                if len(list(classRange)) > 1:
+                    classRange = map(rangeMultiple, classRange)
+                    rangeClasses = '\n'.join(list(classRange))
+                    if 'classOneOf' in locals():
+                        oneOfCollection = '\n'.join(list(classOneOf))
+                        oneOfCollection = '<owl:Class>\n<owl:oneOf rdf:parseType="Collection">\n' + oneOfCollection + '\n</owl:oneOf>\n</owl:Class>'
+                        rangeInfo = '<rdfs:range>\n<owl:Class>\n<owl:unionOf rdf:parseType="Collection">\n' + rangeClasses + '\n' + oneOfCollection + '\n</owl:unionOf>\n</own:Class>\n</rdfs:range>'
+                    else:
+                        rangeInfo = '<rdfs:range>\n<owl:Class>\n<owl:unionOf rdf:parseType="Collection">\n' + rangeClasses + '\n</owl:unionOf>\n</own:Class>\n</rdfs:range>'
+                else:
+                    rangeInfo = list(map(rangeLine, classRange))
+                    rangeInfo = '\n'.join(list(rangeInfo))
+
+            # constraintList += list(rangeInfo)
+
+            if 'classDomain' in locals():
+                if len(list(classDomain)) > 1:
+                    classDomain = map(rangeMultiple, classDomain)
+                    domainClasses = '\n'.join(list(classDomain))
+                    # if classOneOf:
+                    #     oneOfCollection = '\n'.join(list(classOneOf))
+                    #     oneOfCollection = '<owl:Class>\n<owl:oneOf rdf:parseType="Collection">\n' + oneOfCollection + '\n</owl:oneOf>\n</owl:Class>'
+                    #     rangeInfo = '<rdfs:range>\n<owl:Class>\n<owl:unionOf rdf:parseType="Collection">\n' + rangeClasses + '\n' + oneOfCollection + '\n</owl:unionOf>\n</own:Class>\n</rdfs:range>'
+                    # else:
+                    domainInfo = '<rdfs:domain>\n<owl:Class>\n<owl:unionOf rdf:parseType="Collection">\n' + domainClasses + '\n</owl:unionOf>\n</own:Class>\n</rdfs:domain>'
+                else:
+                    domainInfo = list(map(domainLine, classDomain))
+                    domainInfo = '\n'.join(list(domainInfo))
+
+
+        else:
+            otherKeys.append(key)
+
+    # constraintList += list(domainInfo)
+    propertyDescription = [propertyDeclaration, resourceLabel, resourceDescription, resourceType, resourceType2]
+    if 'resourceInstanceList' in locals():
+        resourceInstanceOf = '\n'.join(resourceInstanceList)
+        propertyDescription.append(resourceInstanceOf)
+    if 'resourceSeeList' in locals():
+        resourceSeeAlso = '\n'.join(resourceSeeList)
+        propertyDescription.append(resourceSeeAlso)
+    if 'resourceSubPropertyList' in locals():
+        resourceSubPropertyOf = '\n'.join(resourceSubPropertyList)
+        propertyDescription.append(resourceSubPropertyOf)
+    if 'resourceEquivalentList' in locals():
+        resourceEquivalentProperty = '\n'.join(resourceEquivalentList)
+        propertyDescription.append(resourceEquivalentProperty)
+    if 'domainInfo' in locals():
+        propertyDescription.append(domainInfo)
+    if 'rangeInfo' in locals():
+        propertyDescription.append(rangeInfo)
+
+    propertyDescription.append(propertyDeclarationClosure)
+
+    return propertyDescription, otherKeys, constraintKeys
+
+def classExtractor(lineParsed):
+    otherKeys = []
+
+    try:
+        resourceName = resourceNamer(lineParsed['id'])
+    except:
+        print(lineParsed)
+
+
+    classDeclaration = '<owl:Class rdf:about="' + resourceName + '">'
+    classDeclarationClosure = '</owl:Class>'
+
+    try:
+        resourceLabel = '<rdfs:label xml:lang="en">' + lineParsed['labels']['en']['value'] + '</rdfs:label>'
+    except KeyError:
+        resourceLabel = '<rdfs:label xml:lang="en">no English label available</rdfs:label>'
+
+    try:
+        resourceDescription = '<schema:description xml:lang="en">' + lineParsed['descriptions']['en'][
+            'value'] + '</schema:description>'
+    except KeyError:
+        resourceDescription = '<schema:description xml:lang="en">no English description available</schema:description>'
+
+    # analyse claims
+    # print(lineParsed['claims'].keys())
+    for key in lineParsed['claims']:
+        if key == 'P31':
+            resourceInstanceList = []
+            for i in lineParsed['claims']['P31']:
+                try:
+                    instanceOf = i['mainsnak']['datavalue']['value']['id']
+                    instanceOf = "http://www.wikidata.org/entity/" + instanceOf
+                    resourceInstanceOf = '<rdf:type rdf:resource="' + instanceOf + '"/>'
+                    resourceInstanceList.append(resourceInstanceOf)
+                except:
+                    print(i)
+
+        elif key == 'P279':
+            resourceSubClassList = []
+            for i in lineParsed['claims']['P279']:
+                try:
+                    subClassOf = i['mainsnak']['datavalue']['value']['id']
+                    subClassOf = "http://www.wikidata.org/entity/" + subClassOf
+                    resourceSubClassOf = '<rdfs:subClassOf rdf:resource="' + subClassOf + '"/>'
+                    resourceSubClassList.append(resourceSubClassOf)
+                except:
+                    print(i)
+
+        elif key == 'P1709':
+            resourceEquivalentClassList = []
+            for i in lineParsed['claims']['P1709']:
+                try:
+                    equivalentClass = i['mainsnak']['datavalue']['value']['id']
+                    equivalentClass = "http://www.wikidata.org/entity/" + equivalentClass
+                    resourceEquivalentClassOf = '<rdfs:subClassOf rdf:resource="' + equivalentClass + '"/>'
+                    resourceEquivalentClassList.append(resourceEquivalentClassOf)
+                except:
+                    print(i)
+
+        elif key == 'P2738': #disjointUnionOf
+            resourceDisjointUnionList = []
+            resourceDisjointUnionList.append('<owl:DisjointUnion>')
+            try:
+                disjointUnionClassesList = [disjointUnionClasses(x['datavalue']['value']['id']) for x in i['qualifiers']['P642']]
+                resourceDisjointUnionList +=  disjointUnionClassesList
+                resourceDisjointUnionList.append('</owl:DisjointUnion>')
+            ###account for somevalue/no value
+            except:
+                print(lineParsed['claims'][key])
+
+
+        else:
+            otherKeys.append(key)
+
+    classData = [classDeclaration, resourceLabel, resourceDescription]
+    if 'resourceInstanceList' in locals():
+        resourceInstanceOf = '\n'.join(resourceInstanceList)
+        classData.append(resourceInstanceOf)
+    if 'resourceSubClassList' in locals():
+        resourceSubClassOf = '\n'.join(resourceSubClassList)
+        classData.append(resourceSubClassOf)
+    if 'resourceEquivalentClassList' in locals():
+        resourceEquivalentClassOf = '\n'.join(resourceEquivalentClassList)
+        classData.append(resourceEquivalentClassOf)
+
+
+    classData.append(classDeclarationClosure)
+
+    return classData, otherKeys
+
+    # print(classDeclaration, classDeclarationClosure)
+
+
+# file_name = '/Users/alessandro/Documents/PhD/WD_ontology/four_properties.json'
+def fileAnalyser(file_name):
+    #load classes list
+    classesList = classesGenerator()
+    entitiesAll = []
+    # collect other properties
+    otherKeys = []
+    constraintKeys = []
+
+    # open dumps
+    with bz2.BZ2File(file_name, 'r') as f:
+        # counterI = 0
+        # counterP = 0
+
+        for line in f:
+            # print(counter)
+
+            # if counterI == 3000 and counterP == 300:
+            #     otherKeys = set(otherKeys)
+            #     constraintKeys = set(constraintKeys)
+            #     return entitiesAll, otherKeys, constraintKeys
+            #     break
+
+            try:
+                lineParsed = ujson.loads(line[:-2])
+                entityID = lineParsed['id']
+                if re.match('[P][0-9]{1,}', entityID):
+                    # lineParsed = lineParsed['entities'][propertyId]
+                    # extract resource name
+                    propertyData = propertyExtractor(lineParsed)
+
+                    try:
+                        propertyDescriptionLine = '\n'.join(propertyData[0])
+                        entitiesAll.append(propertyDescriptionLine)
+                        # counterP += 1
+                    except TypeError:
+                        print(propertyData[0])
+
+                    otherKeys += propertyData[1]
+                    constraintKeys += propertyData[2]
+
+                elif entityID in classesList:
+                    # if counterI == 3000:
+                    #     pass
+                    # else:
+                    # lineParsed = lineParsed['entities']['Q5'] ###temporary
+                    classData = classExtractor(lineParsed)
+                    try:
+                        classDescriptionLine = '\n'.join(classData[0])
+                        entitiesAll.append(classDescriptionLine)
+                        # counterI += 1
+                    except TypeError:
+                        print(classData[0])
+
+                    otherKeys += classData[1]
+
+            except ValueError:
+                print(line)
+
+
+
+
+        otherKeys = set(otherKeys)
+        constraintKeys = set(constraintKeys)
+
+        return entitiesAll, otherKeys, constraintKeys
+
+
+def writeOntology(propertyAll):
+    ###write properties
+    with open('WDOntology.owl', 'w') as f:
+        x = fileWriter.OntologyFile(f)
+        x.finalWriter(propertyAll)
+
